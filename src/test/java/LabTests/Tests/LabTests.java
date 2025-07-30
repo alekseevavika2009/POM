4401ba873a4ac13c17c0612017cee13500181399
alekseevavika2009 <153806218+alekseevavika2009@users.noreply.github.com>
2025-07-30 18:49:18 +0300

    Update LabTests.java

--- src/test/java/LabTests/Tests/LabTests.java
+++ src/test/java/LabTests/Tests/LabTests.java
@@ -1,4 +1,49 @@
 package LabTests.Tests;
 
+import LabTests.Pages.LoginPage;
+import LabTests.Pages.RoomPage;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class LabTests {
+    String userLogin = "admin";
+    String password = "password";
+
+    String room = "104";
+    String type = "Twin";
+    String accessible = "true";
+    String price = "500";
+    String roomDetails = "TV";
+
+    @BeforeAll
+    public static void setUp() {
+        open("https://automationintesting.online/admin");
+    }
+
+    @Test
+    public void LoginTest() {
+        LoginPage.waitForPageIsLoaded();
+
+        LoginPage.enterUserName(userLogin);
+        LoginPage.enterPassword(password);
+        LoginPage.clickLoginButton();
+    }
+
+    @Test
+    public void CreateRoomTest() {
+        RoomPage.waitForRoomPageIsLoaded();
+
+        RoomPage.enterRoom(room);
+        RoomPage.enterType(type);
+        RoomPage.enterAccessible(accessible);
+        RoomPage.enterPrice(price);
+        RoomPage.enterRoomDetails(roomDetails);
+
+        RoomPage.clickCreateButton();
+    }
+
+
 }
