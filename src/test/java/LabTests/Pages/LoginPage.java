79dfdb49b9856afbe06ef80e9fe2931baf12ea09
alekseevavika2009 <153806218+alekseevavika2009@users.noreply.github.com>
2025-07-31 12:55:37 +0300

    Update LoginPage.java

--- src/test/java/LabTests/Pages/LoginPage.java
+++ src/test/java/LabTests/Pages/LoginPage.java
@@ -14,20 +14,20 @@ public class LoginPage {
     private static final SelenideElement loginButton = $("#doLogin");
 
     // проверяем корректно ли загрузился заголовок
-    public static void waitForPageIsLoaded() {
+    public void waitForPageIsLoaded() {
         title.shouldBe(visible).shouldHave(text("Login"));
     }
 
     // методы для заполнения поля userName, пароля. Далее, клик
-    public static void enterUserName(String value) {
+    public void enterUserName(String value) {
         userName.setValue(value);
     }
 
-    public static void enterPassword(String value) {
+    public void enterPassword(String value) {
         password.setValue(value);
     }
 
-    public static void clickLoginButton() {
+    public void clickLoginButton() {
         loginButton.click();
     }
 
